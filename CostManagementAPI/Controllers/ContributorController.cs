581679d674fedce3a1badceb28c7579b5f877721
rahultmbi <rahulcse218@gmail.com>
2020-06-13 15:17:51 +0530

    Adding New CostManagementApi

---
+++ CostManagementAPI/Controllers/ContributorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CostManagementAPI.Data;
+using CostManagementAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CostManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContributorController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContributorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contributors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contributor>>> GetProduct()
+        {
+            return await _context.Contributors.ToListAsync();
+        }
+
+        // GET: api/Contributors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contributor>> GetProduct(int id)
+        {
+            var product = await _context.Contributors.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+    }
+}
