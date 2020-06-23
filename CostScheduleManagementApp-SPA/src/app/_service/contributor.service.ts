b50c88858b93a6e89617a04458638e75161bcf37
Venkata Ramaiah Potla <P7111774@NESSINBLR.COM>
2020-06-23 23:11:28 +0530

    commiting Sales

--- CostScheduleManagementApp-SPA/src/app/_service/contributor.service.ts
+++ CostScheduleManagementApp-SPA/src/app/_service/contributor.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Http, Headers } from '@angular/http';
 import { Imprint } from '../_model/Imprint';
+import { SalesTemplates } from '../_model/SalesTemplates';
 
 @Injectable({
   providedIn: 'root'
@@ -36,6 +37,15 @@ export class ContributorService {
 
   getEditionType(){
     return this.http.get<any[]>(this.baseUrl + 'Contributor/getEditionType');
+  }
+
+  saveSalesTemplate(salesTemplate: SalesTemplates){
+    return this.http.post(this.baseUrl + 'Contributor/saveSalesTemplate/'+ salesTemplate, {});
+
+  }
+
+  getSalesTemplates(){
+    return this.http.get<any[]>(this.baseUrl + 'Sales/getSalesTemplates');
   }
 
 }
