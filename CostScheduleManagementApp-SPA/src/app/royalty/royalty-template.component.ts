67ed6fbae8e938b7533b0e99cde4905897de0033
Naresh Reddy Maddireddy <P7112177@NESSINBLR.COM>
2020-06-24 11:12:00 +0530

    Royalty Template

--- CostScheduleManagementApp-SPA/src/app/royalty/royalty-template.component.ts
+++ CostScheduleManagementApp-SPA/src/app/royalty/royalty-template.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContributorService } from '../_service/contributor.service';
+import { AlertifyService } from '../_service/alertify.service';
 
 @Component({
   selector: 'app-royalty-template',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoyaltyTemplateComponent implements OnInit {
 
-  constructor() { }
+  public royallityTemplate: any[];
+  costTemplateTitle: string = "Royalty Template";
+
+  constructor(private contributorService: ContributorService, private alertify: AlertifyService) { 
+
+    
+    this.contributorService.getRoyaltyTypes().subscribe(result => {
+        this.royallityTemplate = result;
+      }, error => console.error(error));
+
+  }
 
   ngOnInit() {
+  }
+
+  saveRoyaltyTemplate(){
+    console.log("data saved");
   }
 
 }
