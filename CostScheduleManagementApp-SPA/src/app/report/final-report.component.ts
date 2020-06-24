75e45fa586436aacaef60f8887a3ccada59ba236
Venkata Ramaiah Potla <P7111774@NESSINBLR.COM>
2020-06-24 23:31:11 +0530

    report page

--- CostScheduleManagementApp-SPA/src/app/report/final-report.component.ts
+++ CostScheduleManagementApp-SPA/src/app/report/final-report.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ContributorService } from '../_service/contributor.service';
+import { AlertifyService } from '../_service/alertify.service';
+import { NgForm } from '@angular/forms';
+import { ISalesTemplates } from '../_model/CostTemplate';
+import { IscheduleTemplate } from '../_model/scheduleTemplate';
 
 @Component({
   selector: 'app-final-report',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinalReportComponent implements OnInit {
 
-  constructor() { }
+
+  public costTemplate: any[];
+  public scheduleTemplate: any[];
+  reportTitle: string = "Cost Calculation Report";
+  public costTemplatesData: ISalesTemplates[] = [];
+
+  constructor(private contributorService: ContributorService, private alertify: AlertifyService) { 
+
+          this.contributorService.getCostTemplateData().subscribe(result => {
+        this.costTemplate = result;
+      }, error => console.error(error));
+
+      this.contributorService.getSecheduleTemplate().subscribe(result => {
+        this.scheduleTemplate = result;
+      }, error => console.error(error));
+  }
 
   ngOnInit() {
   }
+ 
+
 
 }
+
