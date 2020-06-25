5307cb033fd3364dbb93cc1bd704674402f03a3d
Rohit Singh <P7111408@NESSINBLR.COM>
2020-06-25 11:59:03 +0530

    SPA-styling schedule

--- CostScheduleManagementApp-SPA/src/app/schedule/schedule-template.component.ts
+++ CostScheduleManagementApp-SPA/src/app/schedule/schedule-template.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { IscheduleTemplate } from '../_model/scheduleTemplate';
 import { AlertifyService } from '../_service/alertify.service';
 import { ContributorService } from '../_service/contributor.service';
-import { IscheduleTemplate } from '../_model/scheduleTemplate';
 
 @Component({
   selector: 'app-schedule-template',
@@ -10,30 +10,30 @@ import { IscheduleTemplate } from '../_model/scheduleTemplate';
   styleUrls: ['./schedule-template.component.css']
 })
 export class ScheduleTemplateComponent implements OnInit {
-  months;
+  public months: string[] = [];
   scheduleTemplateTitle: string = "Schedule Template";
 
   public scheduleTemplatesData: IscheduleTemplate[] = [];
 
   constructor(private contributorService: ContributorService, private alertify: AlertifyService) { }
 
-  ngOnInit() {  
+  ngOnInit() {
     console.log(this.getscheduleTemplates());
     this.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  } 
+  }
 
-  savescheduleTemplate(scheduleTemplateForm: NgForm){
+  savescheduleTemplate(scheduleTemplateForm: NgForm) {
     console.log("Data Saved!");
   }
 
   getscheduleTemplates() {
     this.contributorService.getSecheduleTemplate().subscribe(results => {
-        results.forEach(scheduleT => {
-            this.scheduleTemplatesData.push(scheduleT);
-        });
-        
+      results.forEach(scheduleT => {
+        this.scheduleTemplatesData.push(scheduleT);
+      });
+
     }, error => {
-        this.alertify.error(error);
+      this.alertify.error(error);
     });
   }
 
